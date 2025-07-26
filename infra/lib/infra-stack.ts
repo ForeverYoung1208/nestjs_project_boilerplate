9a6d9248857456e58e41ac3df0d9961d3e48df41
Ihor S <siafin2010@gmail.com>
2025-07-26 16:20:16 +0200

    fix policies

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -847,18 +847,6 @@ export class AppStack extends cdk.Stack {
           }),
 
           new iam.PolicyStatement({
-            effect: iam.Effect.ALLOW,
-            actions: [
-              'elasticbeanstalk:CreateApplicationVersion',
-              'elasticbeanstalk:UpdateEnvironment',
-            ],
-            resources: [
-              `arn:aws:elasticbeanstalk:${this.region}:${this.account}:application/${projectName}*`,
-              `arn:aws:elasticbeanstalk:${this.region}:${this.account}:environment/${projectName}*/*`,
-            ],
-          }),
-
-          new iam.PolicyStatement({
             actions: [
               's3:PutObject',
               's3:GetObject',
@@ -868,6 +856,18 @@ export class AppStack extends cdk.Stack {
             resources: [
               `arn:aws:s3:::${projectName.toLowerCase()}-eb-artifacts`,
               `arn:aws:s3:::${projectName.toLowerCase()}-eb-artifacts/*`,
+            ],
+          }),
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: [
+              'elasticbeanstalk:CreateApplicationVersion',
+              'elasticbeanstalk:UpdateEnvironment',
+            ],
+            resources: [
+              `arn:aws:elasticbeanstalk:${this.region}:${this.account}:application/${projectName}-application`,
+              `arn:aws:elasticbeanstalk:${this.region}:${this.account}:applicationversion/${projectName}-application/*`,
+              `arn:aws:elasticbeanstalk:${this.region}:${this.account}:environment/${projectName}-application/${projectName}*`,
             ],
           }),
         ],
